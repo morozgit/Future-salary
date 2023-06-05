fab8b71d68d28796f0594b0065113a1ef4c2dadf
morozgit <morozec76@gmail.com>
2023-06-05 23:31:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import requests
+import json
 
 
 payload = {
-    "text": "программист",
+    "text": "Python OR Java",
     "area": "1",
-    "period": "30"
+    "period": "30",
+    # "per_page": 1,
+    
     }
 # url = 'https://api.hh.ru/suggests/vacancy_positions?text=программист'
 url = 'https://api.hh.ru/vacancies'
 response = requests.get(url, params=payload)
 response.raise_for_status()
-hh = response.json()
+hh = response.json()['found']
 print(hh)
+# for i in hh:
+#     print(i['name'])
+# with open("hh.txt", "w") as file:
+#     json.dump(hh, file)
