0140e19a573b9704ec91254c4d81b2c69a691219
morozgit <morozec76@gmail.com>
2023-06-06 18:16:41 +0300

    foo fo salary

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import requests
 import json
 
 
-# def predict_rub_salary():
+def predict_rub_salary(vacancie):
+    url = 'https://api.hh.ru/vacancies?text={0}'.format(vacancie)
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    salary = response.json()['items']
+    for i in salary:
+        print(i['salary'])
 
 
 program_language = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C#', 'C']
-vacancies_language = ()
+vacancies_language = {}
 payload = {
     # "text": "Python",
     "area": "1",
@@ -16,21 +22,17 @@ payload = {
 # url = 'https://api.hh.ru/suggests/vacancy_positions?text=программист'
 for i in program_language:
     url = 'https://api.hh.ru/vacancies?text={0}'.format(i)
-    if i == 'Python':
-        response = requests.get(url, params=payload)
-        response.raise_for_status()
-        python_salary = response.json()['items']
-        for i in python_salary:
-            print(i['salary'])
         # with open("python_salary.txt", "w") as file:
         #     json.dump(python_salary, file)
     response = requests.get(url, params=payload)
     response.raise_for_status()
     hh = response.json()['found']
     print(i)
-#     vacancies_language[i].append(hh)
-# for key, value in vacancies_language.items():
-#     print(key, value)
+    vacancies_language[i] = hh
+for key, value in vacancies_language.items():
+    print(key, value)
+
+predict_rub_salary('Python')
 # program_language['Python'] = hh
 # print(program_language[1])
 # for i in hh:
