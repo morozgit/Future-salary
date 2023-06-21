20f888399244ea7e4a9056b9e16c04e5e2d11db5
morozgit <morozec76@gmail.com>
2023-06-21 11:28:29 +0300

    fix error 403, united table function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 from itertools import count
 
 import requests
@@ -18,58 +19,37 @@ def predict_salary(salary_from, salary_to):
 
 
 def predict_rub_salary_hh(vacancies_hh):
-    vacancy_count = 0
-    average_salary_hh = 0
+    vacancy_count_hh = 0
+    salaries_hh = 0
     for vacancy_hh in vacancies_hh:
         if vacancy_hh['salary']:
             salary_from_hh = vacancy_hh['salary']['from']
             salary_to_hh = vacancy_hh['salary']['to']
-            average_salary_hh += predict_salary(salary_from_hh, salary_to_hh)
-            vacancy_count += 1
+            salaries_hh += predict_salary(salary_from_hh, salary_to_hh)
+            vacancy_count_hh += 1
+    average_salary_hh = salaries_hh // vacancy_count_hh
     try:
-        return vacancy_count, average_salary_hh // vacancy_count
+        return vacancy_count_hh, average_salary_hh
     except ZeroDivisionError:
-        return vacancy_count, 'Salary didn`t find'
+        return vacancy_count_hh, 'Salary didn`t find'
 
 
 def predict_rub_salary_sj(vacancies_sj):
-    vacancy_count = 0
-    average_salary_sj = 0
+    vacancy_count_sj = 0
+    salaries_sj = 0
     for vacancy_sj in vacancies_sj:
         salary_from_sj = vacancy_sj['payment_from']
         salary_to_sj = vacancy_sj['payment_to']
-        average_salary_sj += predict_salary(salary_from_sj, salary_to_sj)
-        vacancy_count += 1
+        salaries_sj += predict_salary(salary_from_sj, salary_to_sj)
+        vacancy_count_sj += 1
+    average_salary_sj = salaries_sj // vacancy_count_sj
     try:
-        return vacancy_count, average_salary_sj // vacancy_count
+        return vacancy_count_sj, average_salary_sj
     except ZeroDivisionError:
-        return vacancy_count, 'Salary didn`t find'
+        return vacancy_count_sj, 'Salary didn`t find'
 
 
-def make_table_hh(vacancies_hh):
-    vacancies_hh_table = []
-    table_headers = [
-        'Язык программирования',
-        'Вакансий найдено',
-        'Вакансий обработано',
-        'Средняя зарплата',
-        ]
-    vacancies_hh_table.append(table_headers)
-    for language, vacancy_information in vacancies_hh.items():
-        vacancies_hh_table.append(
-            [
-                language,
-                vacancy_information.get('vacancies_found'),
-                vacancy_information.get('vacancies_processed'),
-                vacancy_information.get('average_salary'),
-            ]
-        )
-    table = AsciiTable(vacancies_hh_table)
-    table.title = 'HeadHunter Moscow'
-    return table.table
-
-
-def make_table_sj(vacancies_sj):
+def make_table(vacancies, title):
     vacancies_sj_table = []
     table_headers = [
         'Язык программирования',
@@ -78,7 +58,7 @@ def make_table_sj(vacancies_sj):
         'Средняя зарплата',
         ]
     vacancies_sj_table.append(table_headers)
-    for language, vacancy_information in vacancies_sj.items():
+    for language, vacancy_information in vacancies.items():
         vacancies_sj_table.append(
             [
                 language,
@@ -88,7 +68,7 @@ def make_table_sj(vacancies_sj):
             ]
         )
     table = AsciiTable(vacancies_sj_table)
-    table.title = 'SuperJob Moscow'
+    table.title = title
     return table.table
 
 
@@ -101,7 +81,12 @@ def main():
     moskva_id = 1
     amount_of_days = 30
     max_pages_hh = 20
-    amount_of_vacancies_on_page = 100
+    amount_of_vacancies_on_page = 50
+    title_hh = 'HeadHunter Moscow'
+    title_sj = 'SuperJob Moscow'
+    headers_hh = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    }
     for program_language in popular_languages:
         for page in count(0):
             payload = {
@@ -111,7 +96,7 @@ def main():
                 'per_page': amount_of_vacancies_on_page
             }
             url_hh = 'https://api.hh.ru/vacancies'
-            response = requests.get(url_hh, params=payload)
+            response = requests.get(url_hh, headers=headers_hh, params=payload)
             response.raise_for_status()
             vacancies_hh.append(response.json())
             if page >= max_pages_hh:
@@ -126,7 +111,8 @@ def main():
                 'average_salary': average_salary,
             }
             vacancies_language_hh[program_language] = salary_hh_statistics
-    print(make_table_hh(vacancies_language_hh))
+            time.sleep(2)
+    print(make_table(vacancies_language_hh, title_hh))
 
     vacancies_language_sj = {}
     vacancies_sj = []
@@ -166,7 +152,7 @@ def main():
                 'average_salary': average_salary,
             }
             vacancies_language_sj[program_language_sj] = salary_sj_statistics
-    print(make_table_sj(vacancies_language_sj))
+    print(make_table(vacancies_language_sj, title_sj))
 
 
 if __name__ == '__main__':
